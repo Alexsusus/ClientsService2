ecf134306cace2c6e7d99677bbd27b5f11370f2a
Alex <gopazz098@gmail.com>
2022-11-13 17:00:35 +0200

    Repositories Client OwnMethod

--- ClientsService/src/test/java/com/example/clientsservice/repositories/ClientRepositoryTest.java
+++ ClientsService/src/test/java/com/example/clientsservice/repositories/ClientRepositoryTest.java
@@ -23,6 +23,7 @@ public class ClientRepositoryTest {
     static Client b = new Client(0, "b", "b", "b", MALE,
             "b@test.com", null, null, null);
 
+
     @Test
     @Order(1)
     void saveAll() {
@@ -30,29 +31,39 @@ public class ClientRepositoryTest {
         Client b_saved = clientRepository.save(b);
         a.setId(a_saved.getId());
         b.setId(b_saved.getId());
-        assertEquals(a,a_saved);
-        assertEquals(b,b_saved);
+        assertEquals(a, a_saved);
+        assertEquals(b, b_saved);
     }
 
     @Test
     @Order(2)
-    void findAllByCount(){
-        assertEquals(2,clientRepository.findAll().size());
+    void findAllByCount() {
+        assertEquals(2, clientRepository.findAll().size());
     }
 
     @Test
     @Order(3)
-    void findAllByCollection(){
-        List<Client> original = List.of(a,b);
+    void findAllByCollection() {
+        List<Client> original = List.of(a, b);
         List<Client> saved = clientRepository.findAll();
-        assertIterableEquals(original,saved);
+        assertIterableEquals(original, saved);
     }
 
     @Test
     @Order(4)
-    void deleteAll(){
+    void findAllBySurnameNamePatronymic() {
+        List<Client> actual =
+        clientRepository.findAllBySurnameAndNameAndPatronymic(
+                a.getSurname(), a.getName(), a.getPatronymic());
+        System.out.println(actual);
+        assertEquals(a, actual.get(0));
+    }
+
+    @Test
+    @Order(5)
+    void deleteAll() {
         clientRepository.deleteAll();
-        assertEquals(0,clientRepository.findAll().size());
+        assertEquals(0, clientRepository.findAll().size());
     }
 
 }
