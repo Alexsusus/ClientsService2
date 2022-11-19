63f6698370df5ce04f40d838f916e89f22d31b53
Alex <gopazz098@gmail.com>
2022-11-19 20:44:23 +0200

    mustache Users All DZ

--- ClientsService/src/main/java/com/example/clientsservice/controllers/UsersController.java
+++ ClientsService/src/main/java/com/example/clientsservice/controllers/UsersController.java
@@ -3,13 +3,14 @@ package com.example.clientsservice.controllers;
 import com.example.clientsservice.models.User;
 import com.example.clientsservice.services.data.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-
+@Controller
 public class UsersController {
     @Autowired
     private UserService userService;
