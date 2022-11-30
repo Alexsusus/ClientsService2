e7fe2fb5c68ef6e3a53c4a28dd6f6cead97d172b
Alex <gopazz098@gmail.com>
2022-11-30 19:43:41 +0200

    thymeleaf MainController ClientsController clients.html table Test Selected

--- ClientsService/src/main/java/com/example/clientsservice/controllers/ClientUpdateController.java
+++ ClientsService/src/main/java/com/example/clientsservice/controllers/ClientUpdateController.java
@@ -5,8 +5,12 @@ import com.example.clientsservice.services.data.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ClientUpdateController {
@@ -17,6 +21,14 @@ public class ClientUpdateController {
     public String load(@RequestParam("id") Integer id, Model model){
         Client client = clientService.findById(id);
         model.addAttribute("client", client);
+        model.addAttribute("genders", Client.Gender.values());
         return "clientUpdate";
+    }
+
+    @PostMapping("updateClientForm")
+    public ModelAndView updateClientForm(@ModelAttribute Client client){
+        clientService.save(client);
+        return new ModelAndView("redirect:clientUpdate",
+                new ModelMap("id", client.getId()));
     }
 }
