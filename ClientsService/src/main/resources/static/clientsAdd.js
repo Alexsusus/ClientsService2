f0653f47a21bc58f1f40a0db9f6f7d464387841d
Alex <gopazz098@gmail.com>
2022-12-18 15:49:22 +0200

    thymeleaf clients async files ajax clients object

--- ClientsService/src/main/resources/static/clientsAdd.js
+++ ClientsService/src/main/resources/static/clientsAdd.js
@@ -12,16 +12,47 @@ function ajaxSubmitForm() {
     let json = JSON.stringify(Object.fromEntries(formData))
     //
     let request = new XMLHttpRequest()
-    request.open('post','/rest/addClientForm')
+    request.open('post', '/rest/addClientForm')
     request.setRequestHeader('Content-type', 'application/json; charset=utf-8')
     request.send(json);
     request.addEventListener('readystatechange', function () {
         if (request.readyState !== 4)
             return
-        if (request.status===200){
+        if (request.status === 200) {
             let clients = JSON.parse(request.responseText)
-            clients ={'clients': clients}
-
+            clients = {'clients': clients}
+            console.log(clients)
+            const html = `
+             <table>
+        <tr>
+            <th>ID</th>
+            <th>Surname</th>
+            <th>Name</th>
+            <th>Patronymic</th>
+            <th>Gender</th>
+            <th>Email</th>
+        </tr>
+        {{#clients}}
+            <td>{{id}}</td>
+            <td>{{surname}}</td>
+            <td>{{name}}</td>
+            <td>"{{patronymic}}</td>
+            <td>{{gender}}</td>
+            <td>{{email}}</td>
+            <td>
+                <form action="@{/openClientForm}" method="post">
+                    <input type="hidden" name="id" value="{{id}}">
+                    <input type="submit" value="&#8942;">
+                </form>
+            </td>
+        </tr>
+        {{/clients}}
+    </table>
+            `
+            let result = mustache.render(html, clients)
+            let output = document.getElementById('tableOutput')
+            output.innerHTML = result
         }
+        else alert(request.status)
     })
 }
